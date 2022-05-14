36a34a46d3d284aed0599b1dc33620374a2c1886
Artur Valdna <arturvaldna@gmail.com>
2022-05-14 23:43:30 +0300

    Created a new project

---
+++ BankApp/BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Артур Валдна on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
